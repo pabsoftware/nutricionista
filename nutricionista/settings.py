e8e6db3ad9e18b55e8654951264e5d02b872f562
pabsoftware <pabsoftware@outlook.com>
2023-03-11 10:00:52 -0300

    Criado o app usuarios

--- nutricionista/settings.py
+++ nutricionista/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt_BR'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
