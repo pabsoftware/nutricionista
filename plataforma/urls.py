50c4138ee08fea13efda588a32d3cf6f33113371
pabsoftware <pabsoftware@outlook.com>
2023-03-18 21:01:44 -0300

    Criada view do grafico dados paciente

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . views import pacientes, dados_paciente_listar, dados_paciente
+from . views import pacientes, dados_paciente_listar, dados_paciente, grafico_peso
 
 urlpatterns = [
     path('pacientes/', pacientes, name='pacientes'),
     path('dados_paciente/', dados_paciente_listar, name='dados_paciente_listar'),
-    path('dados_paciente/<str:id>/', dados_paciente, name='dados_paciente')
+    path('dados_paciente/<str:id>/', dados_paciente, name='dados_paciente'),
+    path('grafico_peso/<str:id>/', grafico_peso, name="grafico_peso")
 
     
 ]
--- plataforma/views.py
+++ plataforma/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import constants
 from django.contrib import messages
 from . models import Pacientes, DadosPaciente
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 
@@ -97,3 +99,14 @@ def dados_paciente(request, id):
         messages.add_message(request, constants.SUCCESS, 'Dados cadastrado com sucesso')
         return redirect('/dados_paciente/')
         
+
+@login_required(login_url='/auth/logar/')
+@csrf_exempt
+def grafico_peso(request, id):
+    paciente = Pacientes.objects.get(id=id)
+    dados = DadosPaciente.objects.filter(paciente=paciente).order_by("data")
+    pesos = [dado.peso for dado in dados]
+    labels = list(range(len(pesos)))
+    data = {'peso': pesos,
+    'labels': labels}
+    return JsonResponse(data)
